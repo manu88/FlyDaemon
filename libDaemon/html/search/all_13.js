86d7af1aadad90eb6e985056e63fe488cc1bf8ca
Manuel Deneu <manu88@users.noreply.github.com>
2015-12-22 01:21:20 +0100

    doc upload

---
+++ libDaemon/html/search/all_13.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['willterminateconnection',['WillTerminateConnection',['../_dispatch_8h.html#a3006cd40652ccece1889272aa491dbe1a2fda2cc03593633c97c09cb190ea564e',1,'Dispatch.h']]]
+];
